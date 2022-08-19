1340de43e4649142049ce93483478069cc278e4c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-18 19:25:44 -0600

    Finished the knowledge check

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <all-users></all-users>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AllUsers from './components/AllUsers.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AllUsers
   }
 }
 </script>
---
+++ src/components/AllUsers.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="names">
+        <h3 ref="usernames"></h3>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+    export default {
+        mounted () {
+            axios.request({
+                url: `https://reqres.in/api/users`
+            }).then((response) => {
+                for(let i = 0; i < response[`data`][`data`].length; i++) {
+                   this.$refs[`usernames`][`innerHTML`] += response[`data`][`data`][i][`first_name`];
+                }
+                
+            }).catch((error) => {
+                alert(`please, refresh the page`);
+                console.log(error);
+            });
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
